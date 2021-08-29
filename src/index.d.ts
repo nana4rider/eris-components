9ac5b989afc19578f414eb10f7b3843a99cf8c97
Shunichiro Aki <register.develop@nana4.net>
2021-08-29 09:43:24 +0900

    add definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,11 @@
 import * as Eris from 'eris';
-import * as Util from './util';
-import * as Constants from './constants';
 import ActionRow from './classes/ActionRow';
 import Button from './classes/Button';
+import ErisComponentsCollector from './classes/ComponentsCollector';
 import Menu from './classes/Menu';
 import MenuOption from './classes/MenuOption';
+import * as Constants from './constants';
+import * as Util from './util';
 /**
  * The Eris client.
  * @param  {Eris.Client} ErisClient
@@ -17,12 +18,26 @@ declare function Client(
         debug?: boolean;
         invalidClientInstanceError?: boolean;
         ignoreRequestErrors?: boolean;
-    } = {
-        debug: false,
-        invalidClientInstanceError: true,
-        ignoreRequestErrors: false,
     }
-): Eris.Client;
+): ErisComponentsClient;
+
+type Component = ActionRow | Button | Menu;
+type Message = string | any;
+
+declare class ErisComponentsClient extends Eris.Client {
+    sendComponents(channel: string, components: Component | Component[],
+        content: Message, file?: Eris.MessageFile | Eris.MessageFile[]): Promise<any>;
+    editInteraction(resBody: any, components: Component | Component[],
+        content: Message, file?: Eris.MessageFile | Eris.MessageFile[]): Promise<any>;
+    // https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+    replyInteraction(resBody: any, components: Component | Component[], content: Message,
+        options?: { ephemeral: boolean }, type?: number, file?: Eris.MessageFile | Eris.MessageFile[]): Promise<any>;
+    awaitComponents(filter: (body: any) => boolean, channel: string,
+        options?: { time: number }, thisArg?: any): Promise<any>;
+    createComponentsCollector(filter: (body: any) => boolean,
+        channel: string, options: { time: number }, thisArg?: any): ErisComponentsCollector;
+}
+
 declare const _default: {
     Client: typeof Client;
     ActionRow: typeof ActionRow;
@@ -31,5 +46,6 @@ declare const _default: {
     MenuOption: typeof MenuOption;
     Util: typeof Util;
     Constants: typeof Constants;
+    ErisComponentsClient: typeof ErisComponentsClient;
 };
 export = _default;
